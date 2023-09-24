6ba57ef073df3bf33fedc22d0f38ade0143d12c7
Commentsareuseless <maciek.dziedzic7@gmail.com>
2023-09-24 15:50:36 +0200

    Funished schematics and GPIO drv interface

---
+++ Software/drv/PGpio.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file PGpio.cpp
+ * @author Commentsareuseless
+ * @brief 
+ * @version 0.1
+ * @date 2023-09-23
+ *
+ * Copyright: Macieg :)
+ *
+ */
+#include "PGpio.hpp"
---
+++ Software/drv/PGpio.hpp
@@ -0,0 +1,24 @@
+/**
+ * @file PGpio.hpp
+ * @author Commentsareuseless
+ * @brief 
+ * @version 0.1
+ * @date 2023-09-23
+ *
+ * Copyright: Macieg :)
+ *
+ */
+#pragma once
+
+#include <etl/binary.h>
+
+namespace drv
+{
+
+class Gpio
+{
+public:
+  void Set();
+};
+
+} // namespace drv
